4bce23ab0666d2aa2a105a7ea12b330851f31889
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-11 11:58:45 +0530

    Update bucket-and-db.tf

--- bucket-and-db.tf
+++ bucket-and-db.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "terr-bucket-backend"
+  bucket = "terr-bucket-backend-1"
   acl    = "private"
   versioning {
     enabled = true
