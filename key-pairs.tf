6d721987cde37f4b4102dbaa8ab79c8727a3b3b8
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-14 10:10:36 +0530

    Delete key-pairs.tf

--- key-pairs.tf
+++
@@ -1,9 +0,0 @@
-
-resource "tls_private_key" "kp2" {
-  algorithm   = "RSA"
-  rsa_bits = "4096"
-}
-resource "aws_key_pair" "key" {
-  key_name   = "w1"
-  public_key = tls_private_key.kp2.public_key_openssh
-}
